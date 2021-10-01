8a92364ef783474f9885876ab22f2cb485c918de
pothyn <pothyne@outlook.com>
2021-10-01 11:06:55 -0400

    Solution30 Pseudo and Test

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,50 @@
+package baseline;
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+
+        // getFull()
+
+    }
+
+    public String getFull() {
+        String fullString;
+
+        // for 0 to 12 inclusive (index i)
+            // add getRow(i) and new line to fullString
+
+        // return fullString
+    }
+
+    public String getRow(int row) {
+        // initialize rowString
+
+        // for loop from 0 to 12 inclusive (index i)
+            // add spacing(row * i) to rowString
+
+        // return rowString
+    }
+
+    public String spacing(int value) {
+        int count;
+        String spacedNum;
+        // count = getDigitCount(value)
+
+        // for 0 to (5 - count)
+            // add space to spacedNum
+        // add value to spacedNum
+
+        // return spacedNum
+    }
+
+    public int getDigitCount(int value) {
+        int count;
+
+        // while value > 0
+            // divide value by 10
+            // add 1 to count
+
+        // return count
+    }
+}
---
+++ exercise30/src/test/java/baseline/Solution30Test.java
@@ -0,0 +1,32 @@
+package baseline;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution30Test {
+
+    Solution30 app;
+
+    @BeforeEach
+    void init() {
+        app = new Solution30();
+    }
+
+    @Test
+    void testSpacingWithValue1() {
+        assertEquals("   1", app.spacing(1));
+    }
+
+    @Test
+    void testPrintRow() {
+        assertEquals("    1    2    3    4    5    6    7    8    9   10   11   12",app.getRow(1));
+    }
+
+    @Test
+    void countDigits() {
+        assertEquals(2,app.getDigitCount(20);
+    }
+
+}
