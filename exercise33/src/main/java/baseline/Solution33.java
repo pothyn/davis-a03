98239beae8a347ab567b50bfe3b0bf69a90d2892
pothyn <pothyne@outlook.com>
2021-10-02 11:04:31 -0400

    Solution33 Pseudo and Test

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Hunter Davis
+ */
+
+//        Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe,"
+//        or "Ask again later."
+//
+//        Example Output
+//        What's your question?
+//        > Will I be rich and famous?
+//
+//        Ask again later.
+//        Constraint
+//        The value should be chosen randomly using a pseudo random number generator.
+//        Store the possible choices in a list and select one at random.
+
+package baseline;
+
+public class Solution33 {
+
+    public static void main(String[] args) {
+
+        // Prompt for question
+        // Take input but don't store
+
+        // print randomResponseGenerator()
+
+    }
+
+    public String randomResponseGenerator() {
+        // array of strings initialization
+
+        // generate random number
+
+        // return array of strings [index: random number]
+
+    }
+
+}
---
+++ exercise33/src/test/java/baseline/Solution33Test.java
@@ -0,0 +1,23 @@
+package baseline;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution33Test {
+
+    Solution33 app;
+
+    @Test
+    void testRandomResponseGenerator() {
+        // Ah yes, testing the random number generator should end well. I like these 1/4 odds
+        // Testing will not work for the random number generators.
+    }
+
+    @BeforeEach
+    void init() {
+        app = new Solution33();
+    }
+
+}
