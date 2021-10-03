2f28d53f66857be17f67310f06012f24d4340491
pothyn <pothyne@outlook.com>
2021-10-03 11:35:45 -0400

    Solution39

--- exercise39/src/main/java/baseline/Solution39.java
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -1,29 +1,111 @@
 package baseline;
 
+import java.util.*;
+
 public class Solution39 {
+
+    private List<Map<String,String>> people;
+    private static final String FIRST = "First Name";
+    private static final String LAST = "Last Name";
+    private static final String POS = "Position";
+    private static final String SEP_DATE = "Separation Date";
+
 
     public static void main(String[] args) {
 
-        // Create a hashmap<String, String, String, String>
+        Solution39 app = new Solution39();
 
-        // for 0 to 6
-            // addPerson(firstName, lastName, position, separationDate)
+        app.people = new ArrayList<>();
 
-        // print tabularFormat()
+        // Add the given persons to the people List map
+        app.addPreset();
+
+        // Sort the list
+        app.sort();
+
+        // Display the given map
+        app.displayMap();
     }
 
-    public void addPerson(String firstName, String lastName, String position, String separationDate) {
-        // Add a hashmap
+    public String getValue(int a, int b) {
+        // Accessor method mostly used for finding specific values and testing
+        String secondVal;
+        if(b==0)
+            secondVal = FIRST;
+        else if(b==1)
+            secondVal = LAST;
+        else if(b==2)
+            secondVal = POS;
+        else
+            secondVal = SEP_DATE;
+
+        return people.get(a).get(secondVal);
     }
 
-    public String tabularFormat() {
-        String tabForm;
-
-        // add Header to tabForm
-        // for 0 to list length
-            // add the name | position | separation date
-
-        // return tabForm
+    public void addPerson(Map<String, String> person) {
+        people.add(person);
     }
 
+    private void displayMap() {
+        System.out.print("\n\t\t\tName\t  |\t\t\tPosition\t|\tSeparation Date\n");
+        System.out.println("----------------------|---------------------|-------------------");
+        for (Map<String, String> person : people) {
+            System.out.printf("%10s", person.get(FIRST));
+            System.out.printf("%14s", person.get(LAST) + " | ");
+            System.out.printf("%22s", person.get(POS) + " | ");
+            System.out.printf("%15s", person.get(SEP_DATE) + "\n");
+        }
+    }
+
+    public void sort() {
+        Comparator<Map<String, String>> peopleCompare =
+                Comparator.comparing((Map<String, String> person) -> person.get(LAST));
+
+        people.sort(peopleCompare);
+    }
+
+    public void addPreset() {
+//        John	Johnson	        Manager	    2016-12-31
+        addPerson(Map.of(
+                FIRST, "John",
+                LAST, "Johnson",
+                POS, "Manager",
+                SEP_DATE, "2016-12-31"
+        ));
+//        Tou	Xiong	Software Engineer	2016-10-05
+        addPerson(Map.of(
+                FIRST, "Tou",
+                LAST, "Xiong",
+                POS, "Software Engineer",
+                SEP_DATE, "2016-10-05"
+        ));
+//        Michaela	Michaelson	District Manager	2015-12-19
+        addPerson(Map.of(
+                FIRST, "Michaela",
+                LAST, "Michaelson",
+                POS, "District Manager",
+                SEP_DATE, "2015-12-19"
+        ));
+//        Jake	Jacobson	Programmer
+        addPerson(Map.of(
+                FIRST, "Jake",
+                LAST, "Jacobson",
+                POS, "Programmer",
+                SEP_DATE, ""
+        ));
+//        Jacquelyn	Jackson	DBA
+        addPerson(Map.of(
+                FIRST, "Jacquelyn",
+                LAST, "Jackson",
+                POS, "DBA",
+                SEP_DATE, ""
+        ));
+//        Sally	Webber	Web Developer	2015-12-18
+        addPerson(Map.of(
+                FIRST, "Sally",
+                LAST, "Webber",
+                POS, "Web Developer",
+                SEP_DATE, "2015-12-18"
+        ));
+    }
 }
--- exercise39/src/test/java/baseline/Solution39Test.java
+++ exercise39/src/test/java/baseline/Solution39Test.java
@@ -1,7 +1,23 @@
 package baseline;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution39Test {
+
+    Solution39 app;
+
+    @Test
+    void testAddPreset() {
+        app.addPreset();
+        assertEquals("John", app.getValue(0,0));
+    }
+
+    @BeforeEach
+    void init() {
+        app = new Solution39();
+    }
 
 }
