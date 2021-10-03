ff4c70f513e9da8320a3fd5cfca5cf4f626b0919
pothyn <pothyne@outlook.com>
2021-10-03 12:02:22 -0400

    Solution40

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,41 @@
+package baseline;
+
+import java.util.*;
+
+public class Solution40 {
+
+    public static void main(String[] args) {
+
+        // Add the given persons to the people List map
+        addPreset();
+
+        // Sort the list
+        sort();
+
+        // filter the list
+
+        // print filtered list
+    }
+
+    public List<Map<String, String>> filterList(List<Map<String, String>> originalList, String search) {
+        // Create filtered people list map
+        // scan through every person to see if they match
+        // return list of filtered people
+    }
+
+    public void addPerson(Map<String, String> person) {
+        people.add(person);
+    }
+
+    private void displayMap(List<Map<String, String>> people) {
+        // print out everything
+    }
+
+    public void sort() {
+        // Compare the values and sort based on last name
+    }
+
+    public void addPreset() {
+        // Add in all preset people
+    }
+}
