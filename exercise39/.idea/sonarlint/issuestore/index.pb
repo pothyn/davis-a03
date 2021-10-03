3c93bcbcf809b51c72fe530fcd2ee2c6d3fe0272
pothyn <pothyne@outlook.com>
2021-10-03 12:56:31 -0400

    Solution40

---
+++ exercise39/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+7
+gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
+;
+gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
+Z
+*src/test/java/baseline/Solution39Test.java,9\c\9cfed192561dbd9910311d3109c5059822f56e66
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution39.java,0\a\0ad10578190c176782c9bad2cebbe6e6b65ef718
--- exercise40/src/main/java/baseline/Solution40.java
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -1,26 +1,71 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Hunter Davis
+ */
+
 package baseline;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Solution40 {
 
+    private List<Map<String,String>> people;
+    private static final String FIRST = "First Name";
+    private static final String LAST = "Last Name";
+    private static final String POS = "Position";
+    private static final String SEP_DATE = "Separation Date";
+    private static final Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        Solution40 app = new Solution40();
+        Solution40 newValues = new Solution40();
+        String search;
+
+        app.people = new ArrayList<>();
+
         // Add the given persons to the people List map
-        addPreset();
+        app.addPreset();
 
         // Sort the list
-        sort();
+        app.sort();
 
-        // filter the list
+        System.out.print("Enter a search term: ");
+        search = in.nextLine();
 
-        // print filtered list
+        // Display the given map
+        newValues = app.filterList(app.people, search);
+
+        app.displayMap(newValues.people);
     }
 
-    public List<Map<String, String>> filterList(List<Map<String, String>> originalList, String search) {
-        // Create filtered people list map
-        // scan through every person to see if they match
-        // return list of filtered people
+    public Solution40 filterList(List<Map<String, String>> originalList, String search) {
+        Solution40 filteredList = new Solution40();
+
+        for (Map<String, String> person : originalList) {
+            for (String val : person.values()) {
+                if (val.contains(search)) {
+                    filteredList.addPerson(person);
+                }
+            }
+        }
+        return filteredList;
+    }
+
+    public String getValue(int a, int b) {
+        // Accessor method mostly used for finding specific values and testing
+        String secondVal;
+        if(b==0)
+            secondVal = FIRST;
+        else if(b==1)
+            secondVal = LAST;
+        else if(b==2)
+            secondVal = POS;
+        else
+            secondVal = SEP_DATE;
+
+        return people.get(a).get(secondVal);
     }
 
     public void addPerson(Map<String, String> person) {
@@ -28,14 +73,65 @@ public class Solution40 {
     }
 
     private void displayMap(List<Map<String, String>> people) {
-        // print out everything
+        System.out.print("\n\t\t\tName\t  |\t\t\tPosition\t|\tSeparation Date\n");
+        System.out.println("----------------------|---------------------|-------------------");
+        for (Map<String, String> person : people) {
+            System.out.printf("%10s", person.get(FIRST));
+            System.out.printf("%14s", person.get(LAST) + " | ");
+            System.out.printf("%22s", person.get(POS) + " | ");
+            System.out.printf("%15s", person.get(SEP_DATE) + "\n");
+        }
     }
 
     public void sort() {
-        // Compare the values and sort based on last name
+        Comparator<Map<String, String>> peopleCompare =
+                Comparator.comparing((Map<String, String> person) -> person.get(LAST));
+
+        people.sort(peopleCompare);
     }
 
     public void addPreset() {
-        // Add in all preset people
+//        John	Johnson	        Manager	    2016-12-31
+        addPerson(Map.of(
+                FIRST, "John",
+                LAST, "Johnson",
+                POS, "Manager",
+                SEP_DATE, "2016-12-31"
+        ));
+//        Tou	Xiong	Software Engineer	2016-10-05
+        addPerson(Map.of(
+                FIRST, "Tou",
+                LAST, "Xiong",
+                POS, "Software Engineer",
+                SEP_DATE, "2016-10-05"
+        ));
+//        Michaela	Michaelson	District Manager	2015-12-19
+        addPerson(Map.of(
+                FIRST, "Michaela",
+                LAST, "Michaelson",
+                POS, "District Manager",
+                SEP_DATE, "2015-12-19"
+        ));
+//        Jake	Jacobson	Programmer
+        addPerson(Map.of(
+                FIRST, "Jake",
+                LAST, "Jacobson",
+                POS, "Programmer",
+                SEP_DATE, ""
+        ));
+//        Jacquelyn	Jackson	DBA
+        addPerson(Map.of(
+                FIRST, "Jacquelyn",
+                LAST, "Jackson",
+                POS, "DBA",
+                SEP_DATE, ""
+        ));
+//        Sally	Webber	Web Developer	2015-12-18
+        addPerson(Map.of(
+                FIRST, "Sally",
+                LAST, "Webber",
+                POS, "Web Developer",
+                SEP_DATE, "2015-12-18"
+        ));
     }
 }
