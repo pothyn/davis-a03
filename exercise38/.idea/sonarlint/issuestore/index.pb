a9c4bc93d41c60f75dec8cfba5def6dba1d86549
pothyn <pothyne@outlook.com>
2021-10-03 01:11:12 -0400

    Solution39

---
+++ exercise38/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+X
+(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
+7
+gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
+;
+gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
+Z
+*src/test/java/baseline/Solution38Test.java,7\7\770df9759849276849f44798761600ac30209159
+V
+&src/main/java/baseline/Solution38.java,2\8\28f4056acb34e472f5f1a4471a2931afbc699333
---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,29 @@
+package baseline;
+
+public class Solution39 {
+
+    public static void main(String[] args) {
+
+        // Create a hashmap<String, String, String, String>
+
+        // for 0 to 6
+            // addPerson(firstName, lastName, position, separationDate)
+
+        // print tabularFormat()
+    }
+
+    public void addPerson(String firstName, String lastName, String position, String separationDate) {
+        // Add a hashmap
+    }
+
+    public String tabularFormat() {
+        String tabForm;
+
+        // add Header to tabForm
+        // for 0 to list length
+            // add the name | position | separation date
+
+        // return tabForm
+    }
+
+}
